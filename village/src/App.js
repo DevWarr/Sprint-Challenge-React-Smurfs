de19aeaf40772151f2eb0b7d20c7c19fa10f6124
Devin Warrick <dev.warr@gmail.com>
2019-06-14 10:37:52 -0500

    Added a prettierrc file for auto-formatting.

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-import './App.css';
-import SmurfForm from './components/SmurfForm/SmurfForm';
-import Smurfs from './components/Smurfs/Smurfs';
+import "./App.css"
+import SmurfForm from "./components/SmurfForm/SmurfForm"
+import Smurfs from "./components/Smurfs/Smurfs"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            smurfs: []
+        }
+    }
+    // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+    // Notice what your map function is looping over and returning inside of Smurfs.
+    // You'll need to make sure you have the right properties on state and pass them down to props.
+
+    render() {
+        return (
+            <div className="App">
+                <SmurfForm />
+                <Smurfs smurfs={this.state.smurfs} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
